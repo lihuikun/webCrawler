c70554d86b07509b840327ce0e1a7693ddd29f01
Claude Willits <83935370+lihuikun@users.noreply.github.com>
2024-07-02 17:26:46 +0800

    Update index.js

--- src/juejin/index.js
+++ src/juejin/index.js
@@ -5,6 +5,11 @@ const config = {
   url: 'https://juejin.cn/',
   check_url: 'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uid=7187686052052125222'
 }
+const config2 = {
+  cookie: process.env.COOKIE2,
+  url: 'https://juejin.cn/',
+  check_url: 'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uid=7382208359380108863'
+}
 const signIn = async () => {
   const { data } = await axios({
     method: 'post',
@@ -12,6 +17,14 @@ const signIn = async () => {
     headers: {
       'Referer': config.url,
       'Cookie': config.cookie
+    },
+  })
+  const { data } = await axios({
+    method: 'post',
+    url: config2.check_url,
+    headers: {
+      'Referer': config2.url,
+      'Cookie': config2.cookie
     },
   })
   console.log('掘金签到：' + data.err_msg)
